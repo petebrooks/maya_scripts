e28e7e96cec764518d9c2daac527ce2e4330cc2b
Pete Brooks <pete.brooks@tablexi.com>
2017-08-08 18:34:37 -0500

    groupToHiPoly: Dup all descendants

--- group_to_hi_poly.py
+++ group_to_hi_poly.py
@@ -8,16 +8,28 @@ def groupToHiPoly():
     pm.delete(hiPolyGroupName)
   hiPolyGroup = pm.group(name=hiPolyGroupName, world=True, empty=True)
 
-  for shape in pm.listRelatives(loPolyGroup, children=True):
-    dup = pm.duplicate(shape,
-                       name = toHiPolyName(shape),
-                       returnRootsOnly=True,
-                       upstreamNodes=True)
-    pm.parent(dup, hiPolyGroup)
-    pm.polySmooth(dup, divisions=3)
+  for node in pm.listRelatives(loPolyGroup, allDescendents=True, shapes=True):
+    transform = getTransform(node)
+    if transform:
+      dup = pm.duplicate(transform,
+                         name=toHiPolyName(transform),
+                         returnRootsOnly=True,
+                         upstreamNodes=True)
+      pm.parent(dup, hiPolyGroup)
+      pm.polySmooth(dup, divisions=3)
 
 def toHiPolyName(loPoly):
   baseName = str(loPoly).replace("_lo", "")
   return baseName + "_hi"
+
+# def isPolyMeshTransform(node):
+#   node.getShape() and node.getShape().nodeType() == "mesh"
+
+def getTransform(shape):
+  if shape.nodeType() == "mesh":
+    parents = pm.listRelatives(shape, parent=True, type="transform")
+    return parents[0]
+  else:
+    return None
 
 groupToHiPoly()
