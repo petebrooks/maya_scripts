5797ba7d4457f14f47909d5290b206720c97a156
Pete Brooks <lophochroa@gmail.com>
2019-08-24 15:11:54 -0500

    Add regnamer.fixFBXNames

--- core/regnamer.py
+++ core/regnamer.py
@@ -2,6 +2,7 @@
 # (The g is silent.)
 
 import re
+import pymel.core as pm
 
 def rename_from_parent(mesh, suffix):
   parent = mesh.listRelatives(parent=True)[0]
@@ -11,5 +12,9 @@ def rename_from_parent(mesh, suffix):
 def mesh_basename(mesh):
   return re.match(r"(.+)_(?:Geo|(?:s_)?lo).*", str(mesh)).group(1)
 
-for mesh in pm.selected():
-  rename_from_parent(mesh, "_Cap")
+def fixFBXNames(meshes=None):
+  meshes = meshes or pm.selected()
+  for mesh in meshes:
+    newName = re.match(r"(.+?)FBX.*", str(mesh)).group(1)
+    print "Renaming %s to %s" % (mesh, newName)
+    pm.rename(mesh, newName)
